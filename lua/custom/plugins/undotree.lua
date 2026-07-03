8cbee1f187daa06a774867aae96b5344ae2c50af
Mitchell Munn <munnmitchell@gmail.com>
2026-07-03 12:57:43 +0930

    undotree config

---
+++ lua/custom/plugins/undotree.lua
@@ -0,0 +1,14 @@
+return {
+  'jiaoshijie/undotree',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {},
+  keys = {
+    {
+      '<leader>u',
+      function()
+        require('undotree').toggle()
+      end,
+      desc = 'Toggle UndoTree',
+    },
+  },
+}
